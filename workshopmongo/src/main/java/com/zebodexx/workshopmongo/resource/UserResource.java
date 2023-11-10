b8d0ff5e0d8e016c3caab12712034cee18b2588a
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-10 16:37:45 -0300

    USANDO PADRAO DTO PARA RETORNAR USUARIOS

--- workshopmongo/src/main/java/com/zebodexx/workshopmongo/resource/UserResource.java
+++ workshopmongo/src/main/java/com/zebodexx/workshopmongo/resource/UserResource.java
@@ -1,6 +1,8 @@
 package com.zebodexx.workshopmongo.resource;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zebodexx.workshopmongo.domain.User;
+import com.zebodexx.workshopmongo.dto.UserDTO;
 import com.zebodexx.workshopmongo.services.UserService;
 
 @RestController
@@ -19,9 +22,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>>findAll()
+	public ResponseEntity<List<UserDTO>>findAll()
 	{
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
