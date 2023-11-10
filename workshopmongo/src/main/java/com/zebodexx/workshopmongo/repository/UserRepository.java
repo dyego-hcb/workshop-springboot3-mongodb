a4cdf408ce0120930cbf3adfa559a5bba2c83f1a
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-10 16:19:20 -0300

    CONEXAO AO MONGODB COM REPOSITORY E SERVICE

---
+++ workshopmongo/src/main/java/com/zebodexx/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.zebodexx.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zebodexx.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
---
+++ workshopmongo/src/main/java/com/zebodexx/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.zebodexx.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zebodexx.workshopmongo.domain.User;
+import com.zebodexx.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll()
+	{
+		return repo.findAll();
+	}
+}
