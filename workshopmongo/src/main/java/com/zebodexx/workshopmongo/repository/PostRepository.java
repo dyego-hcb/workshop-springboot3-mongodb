1d868ce402d1db96da12d254f74723effbe71185
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-12 10:59:38 -0300

    CRIANDO ENTITY POST COM USER ALINHADO

---
+++ workshopmongo/src/main/java/com/zebodexx/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.zebodexx.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zebodexx.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
