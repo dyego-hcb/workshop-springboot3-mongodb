fd78f4b667c16b645b3297e00dd10f4405f36de8
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-13 00:00:26 -0300

    OBTENDO POST POR ID

---
+++ workshopmongo/src/main/java/com/zebodexx/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.zebodexx.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zebodexx.workshopmongo.domain.Post;
+import com.zebodexx.workshopmongo.repository.PostRepository;
+import com.zebodexx.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id)
+	{
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found !!"));
+	}
+}
