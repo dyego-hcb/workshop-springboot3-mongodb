65c6670b9d1038f2854071ebee513b7adcf0b69c
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-13 00:12:50 -0300

    ACRESCENTANDO COMENTARIOS AOS POSTS

--- workshopmongo/src/main/java/com/zebodexx/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/zebodexx/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.zebodexx.workshopmongo.domain.Post;
 import com.zebodexx.workshopmongo.domain.User;
 import com.zebodexx.workshopmongo.dto.AuthorDTO;
+import com.zebodexx.workshopmongo.dto.CommentDTO;
 import com.zebodexx.workshopmongo.repository.PostRepository;
 import com.zebodexx.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Sao Paulo. Abracos!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje !", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite ", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia !", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
--- workshopmongo/src/main/java/com/zebodexx/workshopmongo/domain/Post.java
+++ workshopmongo/src/main/java/com/zebodexx/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.zebodexx.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.zebodexx.workshopmongo.dto.AuthorDTO;
+import com.zebodexx.workshopmongo.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post implements Serializable{
@@ -20,6 +23,9 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
+
 	
 	public Post()
 	{
@@ -75,6 +81,10 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
