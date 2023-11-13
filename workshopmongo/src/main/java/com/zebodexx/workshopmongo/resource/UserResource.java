1794fccafb46545bd4d19ca19f2b548506aa9705
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-11-12 23:54:12 -0300

    ENDPOIT PARA RETORNAR OS POSTS DE UM USUARIO

--- workshopmongo/src/main/java/com/zebodexx/workshopmongo/resource/UserResource.java
+++ workshopmongo/src/main/java/com/zebodexx/workshopmongo/resource/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.zebodexx.workshopmongo.domain.Post;
 import com.zebodexx.workshopmongo.domain.User;
 import com.zebodexx.workshopmongo.dto.UserDTO;
 import com.zebodexx.workshopmongo.services.UserService;
@@ -44,18 +45,24 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<UserDTO> update(@RequestBody UserDTO objDto, @PathVariable String id) {
 		User obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
